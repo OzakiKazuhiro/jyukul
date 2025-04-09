4de7ac5b75534455d57c79590804816ff0d1ee12
OzakiKazuhiro <sciencegreen1110@gmail.com>
2025-04-09 23:28:42 +0900

    コード整理

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -8,19 +8,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
-
 // Googleアカウントによるログイン処理
 Route::get('/external/auth/google', [GoogleLoginController::class, 'redirectToGoogle'])->name('google.login');
 Route::get('/external/auth/google/callback', [GoogleLoginController::class, 'handleGoogleCallback'])->name('google.callback');
-
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -43,8 +33,8 @@ Route::get('/',[ShopController::class, 'index'])->name('shop.index');
 // Shop
 Route::middleware('auth')->group(
     function () {
-        Route::get('/shop/create',[ShopController::class, 'create'])->name('shop.create'); 
-        Route::post('/shop/store',[ShopController::class, 'store'])->name('shop.store');  
+        Route::get('/shop/create',[ShopController::class, 'create'])->name('shop.create');
+        Route::post('/shop/store',[ShopController::class, 'store'])->name('shop.store');
         Route::get('/shop/edit/{id}',[ShopController::class, 'edit'])->name('shop.edit');
         Route::post('/shop/update',[ShopController::class, 'update'])->name('shop.update');
         Route::delete('/shop/delete/{id}',[ShopController::class, 'destroy'])->name('shop.destroy');
@@ -53,7 +43,7 @@ Route::middleware('auth')->group(
 Route::get('/shop/{id}', [ShopController::class, 'detail'])->name('shop.detail');
 Route::get('/shop/user/{userId}', [ShopController::class, 'indexByUser'])->name('shop.indexByUser');
 
-// Review
+
 Route::middleware('auth')->group(
     function () {
         Route::get('/review/create/shop/{id}',[ReviewController::class, 'create'])->name('review.create');
